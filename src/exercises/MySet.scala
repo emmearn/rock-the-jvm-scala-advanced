f3dfcb72f5899518d7ea48cafd773d4ff058d636
Marco Arnone <marcoarnone@easy-network.it>
2019-01-21 12:53:48 +0100

    implementing others methods of sets

--- src/exercises/MySet.scala
+++ src/exercises/MySet.scala
@@ -3,39 +3,82 @@ package exercises
 import scala.annotation.tailrec
 
 trait MySet[A] extends (A => Boolean) {
-  def apply(elem: A): Boolean =
-    contains(elem)
+  def apply(elem: A): Boolean = contains(elem)
 
   def contains(elem: A): Boolean
   def +(elem: A): MySet[A]
-  def ++(anotherSet: MySet[A]): MySet[A]
+  def ++(anotherSet: MySet[A]): MySet[A] // union
 
   def map[B](f: A => B): MySet[B]
   def flatmap[B](f: A => MySet[B]): MySet[B]
   def filter(predicate: A => Boolean): MySet[A]
   def foreach(f: A => Unit): Unit
+
+  def -(elem: A): MySet[A]
+  def --(anotherSet: MySet[A]): MySet[A] // difference
+  def &(anotherSet: MySet[A]): MySet[A] // intersection
+
+  def unary_! : MySet[A]
 }
 
 class EmptySet[A] extends MySet[A] {
-  def contains(elem: A): Boolean =
-    false
+  def contains(elem: A): Boolean = false
+  def +(elem: A): MySet[A] = new NonEmptySet[A](elem, this)
+  def ++(anotherSet: MySet[A]): MySet[A] = anotherSet
 
-  def +(elem: A): MySet[A] =
-    new NonEmptySet[A](elem, this)
-
-  def ++(anotherSet: MySet[A]): MySet[A] =
-    anotherSet
-
-  def map[B](f: A => B): MySet[B] =
-    new EmptySet[B]
-
-  def flatmap[B](f: A => MySet[B]): MySet[B] =
-    new EmptySet[B]
-
+  def map[B](f: A => B): MySet[B] = new EmptySet[B]
+  def flatmap[B](f: A => MySet[B]): MySet[B] = new EmptySet[B]
   def filter(predicate: A => Boolean): MySet[A] = this
+  def foreach(f: A => Unit): Unit = ()
 
-  def foreach(f: A => Unit): Unit =
-    ()
+  def -(elem: A): MySet[A] = this
+  def --(anotherSet: MySet[A]): MySet[A] = this
+  def &(anotherSet: MySet[A]): MySet[A] = this
+
+  def unary_! : MySet[A] = new PropertyBasedSet[A](_ => true)
+}
+
+/* class AllInclusiveSet[A] extends MySet[A] {
+  def contains(elem: A): Boolean = true
+  def +(elem: A): MySet[A] = this
+  def ++(anotherSet: MySet[A]): MySet[A] = this
+
+  def map[B](f: A => B): MySet[B] = ???
+  def flatmap[B](f: A => MySet[B]): MySet[B] = ???
+  def filter(predicate: A => Boolean): MySet[A] = ???
+  def foreach(f: A => Unit): Unit = ???
+
+  def -(elem: A): MySet[A] = ???
+  def --(anotherSet: MySet[A]): MySet[A] = filter(!anotherSet)
+  def &(anotherSet: MySet[A]): MySet[A] = filter(anotherSet)
+
+  def unary_! : MySet[A] = new EmptySet[A]
+} */
+
+// all elements of type A which satisfy a property
+// { x in A | property(x) }
+class PropertyBasedSet[A](property: A => Boolean) extends MySet[A] {
+  def contains(elem: A): Boolean = property(elem)
+
+  // { x in A | property(x) } + element = { x in A | property(x) || x == element }
+  def +(elem: A): MySet[A] = new PropertyBasedSet[A](x => property(x) || x == elem)
+
+  // { x in A | property(x) } ++ set = { x in A | property(x) || set contains x }
+  def ++(anotherSet: MySet[A]): MySet[A] = new PropertyBasedSet[A](x => property(x) || anotherSet(x))
+
+  // all integers => (_ % 3) => [0 1 2 3]
+  def map[B](f: A => B): MySet[B] = politelyFail
+  def flatmap[B](f: A => MySet[B]): MySet[B] = politelyFail
+  def filter(predicate: A => Boolean): MySet[A] = new PropertyBasedSet[A](x => property(x) && predicate(x))
+  def foreach(f: A => Unit): Unit = politelyFail
+
+  def -(elem: A): MySet[A] = filter(x => x != elem)
+  def --(anotherSet: MySet[A]): MySet[A] = filter(!anotherSet)
+  def &(anotherSet: MySet[A]): MySet[A] = filter(anotherSet)
+
+  def unary_! : MySet[A] = new PropertyBasedSet[A](x => !property(x))
+
+  def politelyFail = throw new IllegalArgumentException("Really deep rabbit hole!")
 }
 
 class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
@@ -53,14 +96,9 @@ class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
     [] ++ [4 5] + 1 + 2 + 3 =
     [4 5] + 1 + 2 + 3 = [4, 5, 1, 2, 3]
    */
-  def ++(anotherSet: MySet[A]): MySet[A] =
-    tail ++ anotherSet + head // tail.++(anotherSet + head)
-
-  def map[B](f: A => B): MySet[B] =
-    (tail map f) + f(head)
-
-  def flatmap[B](f: A => MySet[B]): MySet[B] =
-    (tail flatmap f) ++ f(head)
+  def ++(anotherSet: MySet[A]): MySet[A] = tail ++ anotherSet + head // tail.++(anotherSet + head)
+  def map[B](f: A => B): MySet[B] = (tail map f) + f(head)
+  def flatmap[B](f: A => MySet[B]): MySet[B] = (tail flatmap f) ++ f(head)
 
   def filter(predicate: A => Boolean): MySet[A] = {
     val filteredTail = tail filter predicate
@@ -73,6 +111,18 @@ class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
     f(head)
     tail foreach f
   }
+
+  def -(elem: A): MySet[A] =
+    if(head == elem) tail
+    else tail - elem + head
+
+  def --(anotherSet: MySet[A]): MySet[A] =
+    filter(!anotherSet) // x => !anotherSet.contains(x)
+
+  def &(anotherSet: MySet[A]): MySet[A] =
+    filter(anotherSet) // x => anotherSet.contains(x) => intersection = filtering
+
+  def unary_! : MySet[A] = new PropertyBasedSet[A](x => !this.contains(x))
 }
 
 object MySet {
@@ -96,4 +146,6 @@ object MySet {
 object MySetPlayground extends App {
   val s = MySet(1, 2, 3, 4)
   s + 5 ++ MySet(-1, -2) + 3 flatmap (x => MySet(x, x * 10)) filter(_ % 2 == 0) foreach println
+
+  
 }
