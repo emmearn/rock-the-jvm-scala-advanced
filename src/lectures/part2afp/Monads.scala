db14ac93940ae7d5e3bed57c3063bc9d52a7406a
Marco Arnone <marcoarnone@easy-network.it>
2019-01-22 10:05:33 +0100

    monads exercises

--- src/lectures/part2afp/Monads.scala
+++ src/lectures/part2afp/Monads.scala
@@ -57,4 +57,47 @@ object Monads extends App {
   }
 
   println(attempt)
+
+  class Lazy[+A](value: => A) {
+    private lazy val internalValue = value
+
+    def use: A = value
+    def flatMap[B](f: (=> A) => Lazy[B]): Lazy[B] = f(internalValue)
+  }
+
+  object Lazy {
+    def apply[A](value: => A): Lazy[A] = new Lazy(value)
+  }
+
+  val lazyInstance = Lazy {
+    println("Today I don't feel like doing anything")
+    42
+  }
+
+  println(lazyInstance.use)
+
+  val flatMappedInstance = lazyInstance.flatMap(x => Lazy {
+    10 * x
+  })
+
+  val flatMappedInstance2 = lazyInstance.flatMap(x => Lazy {
+    10 * x
+  })
+
+  flatMappedInstance.use
+  flatMappedInstance2.use
+
+  /*
+    left-identity
+    unit.flatMap(f) = f(v)
+    Lazy(v).flatMap(f) = f(v)
+
+    right-identity
+    l.flatMap(unit) = l
+    Lazy(v).flatMap(x => Lazy(x)) = (Lazy(v)
+
+    associativity: l.flatMap(f).flatMap(g) = l.flatMap(x => f(x).flatMap(g))
+    Lazy(v).flatMap(f).flatMap(g) = f(v).flatMap(g)
+    Lazy(v).flatMap(x => f(x).flatMap(g)) = f(v).flatMap(g)
+   */
 }
