1146bbaf9b8996c068d27238e6396e674eea8d16
Marco Arnone <marcoarnone@easy-network.it>
2019-01-28 18:18:31 +0100

    reflection advanced

--- src/lectures/part5fs/Reflection.scala
+++ src/lectures/part5fs/Reflection.scala
@@ -27,4 +27,47 @@ object Reflection extends App {
 
   val method = reflected.reflectMethod(methodSymbol)
   method.apply()
+
+  // type erasure
+
+  // pp #1 differentiate types at runtime
+  val numbers = List(1, 2, 3)
+
+  numbers match {
+    case listOfStrings: List[String] => println("list of strigns")
+    case listOfNumbers: List[Int] => println("list of numbers")
+  }
+
+  // pp #2 limitations on overloads
+  def processList(list: List[Int]): Int = 42
+  // def processList(list: List[String]): Int = 45
+
+  // TypeTags
+  import ru._
+
+  val ttag = typeTag[Person]
+  println(ttag.tpe)
+
+  class MyMap[K, V]
+  def getTypeArguments[T](value: T)(implicit typeTag: TypeTag[T]) = typeTag.tpe match {
+    case TypeRef(_, _, typeArguments) => typeArguments
+    case _ => List()
+  }
+
+  val myMap = new MyMap[Int, String]
+  val typeArgs = getTypeArguments(myMap) // (typeTag: TypeTag[MyMap[Int, String]])
+  println(typeArgs)
+
+  def isSubtype[A, B](implicit ttagA: TypeTag[A], ttagB: TypeTag[B]): Boolean = {
+    ttagA.tpe <:< ttagB.tpe
+  }
+
+  class Animal
+  class Dog extends Animal
+  println(isSubtype[Dog, Animal])
+
+  val anotherMethodSymbol = typeTag[Person].tpe.decl(ru.TermName(methodName)).asMethod
+
+  val sameMethod = reflected.reflectMethod(anotherMethodSymbol)
+  sameMethod.apply()
 }
